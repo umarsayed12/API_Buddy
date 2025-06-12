d83881a40d927e13b1ba44db27054b445876396f
Umar Khursheed <umarkhursheed979@gmail.com>
2025-06-12 12:03:29 +0530

    Implemented upload and test functionalities in backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import uploadRoute from "./routes/uploadRoute.js";
+import testRoute from "./routes/testRoute.js";
+import aiRoute from "./routes/aiRoute.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import dotenv from "dotenv";
+import dbConnect from "./database/dbConnect.js";
+import historyRoute from "./routes/historyRoute.js";
+dotenv.config({});
+const app = express();
+dbConnect();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+  })
+);
+
+app.use("/api/upload", uploadRoute);
+app.use("/api/test", testRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
